dc75f5eb33631d2253978a3a9b3a0ae08da3da5e
Franklin <frankpirapora@hotmail.com>
2018-09-19 20:30:49 -0300

    Sofreu alteraçao

--- src/helogit/HeloGit.java
+++ src/helogit/HeloGit.java
@@ -3,7 +3,9 @@ package helogit;
 public class HeloGit {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+    
+        System.out.println("Hello Git");
+                
     }
     
 }
