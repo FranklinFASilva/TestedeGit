ef565b97f663284a82e609ee8b317b5a05b1e925
Franklin <frankpirapora@hotmail.com>
2018-09-19 20:37:31 -0300

    Mais um commit

--- src/helogit/HeloGit.java
+++ src/helogit/HeloGit.java
@@ -5,7 +5,8 @@ public class HeloGit {
     public static void main(String[] args) {
     
         System.out.println("Hello Git");
-                
+        
+        int a = 5;        
     }
     
 }
